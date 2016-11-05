111f32c1eefe874d01103cf926b44c9b2a6b5415
tcabaj <tomcab95@gmail.com>
2016-11-05 19:16:17 +0100

    Manager1

--- src/main/java/com/project/service/IFilmM.java
+++ src/main/java/com/project/service/IFilmM.java
@@ -6,15 +6,16 @@ import com.project.domain.Film;
 
 public interface IFilmM {
 	
-	public boolean add_kategorie(Film film);
-	
-	public boolean add_all_pharmacies(List<Film> film);
+	public boolean add_film(Film film);	
+	public boolean add_all_film(List<Film> film);
 		
 	public List<Film> get_all_film();
 	
 	public boolean update_film(String sfilm, String nfilm);
 
-	public void clear_kategoria();
-	public boolean clear_kategoria(Film film);
+	public void clear_film();
+	public boolean clear_film(Film film);
 	
+	public void set_kategoria_for_film(Film film, String kategoria_nazwa);
+	public List<Film> get_all_film_for_kategori(String nazwa);
 }
--- src/main/java/com/project/service/IKategoriaM.java
+++ src/main/java/com/project/service/IKategoriaM.java
@@ -6,13 +6,13 @@ import com.project.domain.Kategoria;
 
 public interface IKategoriaM {
 
-	public boolean add_kategorie(Kategoria kategoria);
+	public boolean add_kategoria(Kategoria kategoria);
 	
 	public List<Kategoria> get_all_kategorie();
 	
 	public boolean update_kategoria(String nazwa, String nnazwa);
 
-	public boolean add_all_pharmacies(List<Kategoria> kategoria);
+	public boolean add_all_kategoria(List<Kategoria> kategoria);
 	
 	public void clear_kategoria();
 	
